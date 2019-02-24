872677541eecaa36421d96d3a97dc60927b9c000
Thomas Siskos <thomas.siskos91@gmail.com>
2019-02-24 15:45:19 +0100

    minor fix

--- eda/get_important_variables.sh
+++ eda/get_important_variables.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-tail -n 250 random_forest.log | grep -o -P '(?<=[0-9\s]\.\s)(.*?)(\s)' | head -n 20
+tail -n 250 logs/random_forest.log | grep -o -P '(?<=[0-9\s]\.\s)(.*?)(\s)' | head -n 20
