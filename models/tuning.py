568c5152c7b5593eeaef0a70e11ae824de5adc1a
Thomas Siskos <thomas.siskos91@gmail.com>
2019-03-06 22:06:05 +0100

    Add more variety to crossover.

--- models/tuning.py
+++ models/tuning.py
@@ -215,34 +215,39 @@ class GeneticAlgorithm:
                 if np.random.random() < reset_prob:
                     samples = self.__get_sample()
                     sample, sample_y, sample_p, oob, oob_y, oob_p = samples
-    
+
                 # Determine fitness in a parallelized pool of workers
                 results = parallel(delayed(self.__get_fitness)(
                     sample, sample_y, sample_p, beta)
                     for beta in tqdm(self.pool, desc="Iteration"))
-    
+
                 results = np.array(results)
                 self.fitness = results[:, 0]
                 self.cutoffs = results[:, 1]
-    
+
                 # Save best candidate
                 fitness_idx = np.argsort(-self.fitness)
-                parents_idx = fitness_idx[:self.num_parents]
+                winners_idx = fitness_idx[:self.num_parents]
+                p = (self.population_size - 1 - fitness_idx) / fitness_idx.sum()
+
                 self.__update_history(fitness_idx, oob, oob_y, oob_p)
-    
+
                 # Create new pool
-                self.pool = self.pool[parents_idx, :]
-    
+                pool = self.pool[winners_idx, :]
+
                 # Crossover
-                while self.pool.shape[0] < self.population_size:
-                    parent_a_idx = np.random.choice(range(self.num_parents))
-                    parent_b_idx = np.random.choice(range(self.num_parents))
-    
+                while pool.shape[0] < self.population_size:
+                    parent_a_idx = np.random.choice(winners_idx)
+                    parent_b_idx = np.random.choice(
+                        range(self.population_size), p=p)
+
                     candidate = self.crossover(parent_a_idx, parent_b_idx)
                     if np.random.random() < self.prob_mutation:
                         candidate = self.__mutate(candidate)
-    
-                    self.pool = np.append(self.pool, [candidate], axis=0)
+
+                    pool = np.append(pool, [candidate], axis=0)
+
+                self.pool = pool
 
         # Determine Solution with best OOB-fitness
         opt_idx = np.argmax(self.history["oob_fitness"])
